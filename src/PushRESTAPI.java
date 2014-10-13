9b2aaf6981d42577634ad3a27dce2cca6dd80f67
asow123@gmail.com <asow123@gmail.com>
2014-10-12 21:47:20 -0400

    Added getUserFavorite method in PushRESTAPI.java

--- src/PushRESTAPI.java
+++ src/PushRESTAPI.java
@@ -20,16 +20,30 @@ public class PushRESTAPI {
             getData = data.sendGet("http://experiencepush.com/csp_portal/rest/?PUSH_ID=123&call=getAllListings");
         } catch(IOException ioe){
 
-            System.out.println("I/O error occured: "+ioe);
+            System.out.println("I/O error occurred: "+ioe);
         }
         JSONArray JSON = new JSONArray(getData);
 
         return  JSON;
     }
 
+    public JSONArray getUserFavorites(String uuid)throws Exception{
+        String getData = null;
+        try {
+            getData = data.sendGet("http://experiencepush.com/csp_portal/rest/?PUSH_ID=123&call=getUserFavorites&uuid="+uuid);
+        }catch(IOException ioe){
+            System.out.println("I/O error occurred: "+ioe);
+        }
+        JSONArray JSON = new JSONArray(getData);
+
+        return JSON;
+    }
+
 
    public static void main(String[] args) throws Exception{
        PushRESTAPI api = new PushRESTAPI();
-       System.out.println(api.getAllListings());
+       //System.out.println(api.getAllListings());
+       //System.out.println(api.getUserFavorites("901292DF-C96A-4F19-AF3A-5A1893C41DF3"));
+
    }
 }
